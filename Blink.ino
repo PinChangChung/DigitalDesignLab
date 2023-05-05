4ce8ae25eebed7bd260bc2787cd0e662134f9f71
PinChangChung <130741403+PinChangChung@users.noreply.github.com>
2023-05-05 15:07:50 +0800

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,14 +1,16 @@
 void setup() {
-  //pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
+
 void loop() {
-  int sensorRead = 0;
-  sensorRead = analogRead(A5);//40~160
-  Serial.println(sensorRead);
-  if(sensorRead<40)sensorRead=40;
-  else if(sensorRead>160)sensorRead=160;
-  int lightblink = map(sensorRead,40,160,0,255);
-  analogWrite(3,lightblink);
+  int sensorValue = 0;
+  sensorValue = analogRead(A0);
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(sensorValue);                       // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(sensorValue);                       // wait for a second
+  Serial.println(sensorValue);
   delay(1);
+  
 }
